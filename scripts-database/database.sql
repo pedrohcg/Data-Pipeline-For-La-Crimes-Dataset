960032beb562755a27dcc9ce9968d039f88885d0
pedrohcg <ph-cg@hotmail.com>
2024-04-15 17:22:28 -0300

    changing database dockerfile

--- scripts-database/database.sql
+++ scripts-database/database.sql
@@ -8,7 +8,11 @@ RESTORE DATABASE [crimes_la] FROM
 DISK = N'/src/crimes_la_1.bak',
 DISK = N'/src/crimes_la_2.bak',
 DISK = N'/src/crimes_la_3.bak',
-DISK = N'/src/crimes_la_4.bak'
+DISK = N'/src/crimes_la_4.bak',
+DISK = N'/src/crimes_la_5.bak',
+DISK = N'/src/crimes_la_6.bak',
+DISK = N'/src/crimes_la_7.bak',
+DISK = N'/src/crimes_la_8.bak'
 WITH  FILE = 1,  MOVE N'crimes_la' TO N'/var/opt/mssql/data/crimes_la.mdf',
 MOVE N'crimes_la_log' TO N'/var/opt/mssql/data/crimes_la_log.ldf',  NOUNLOAD,  STATS = 5
 
